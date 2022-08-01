0a0acce6e09b5dc432a52b6ad3d76f5d2e31341c
Edlyn Evangelista <107823538+edlyn-e@users.noreply.github.com>
2022-08-02 08:39:02 +0930

    adds projects.js to fill with project info

---
+++ app/src/server/projects.js
@@ -0,0 +1,10 @@
+const projects = [
+    {
+        title: "Project Title",
+        description: "Lorem ipsum",
+        image: "",
+        links: { code: "", live: "" },
+    },
+];
+
+export default projects;

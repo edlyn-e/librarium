f26347c1f94281a75c13373cd377fe6b58a3c943
Edlyn Evangelista <107823538+edlyn-e@users.noreply.github.com>
2022-08-18 10:10:50 +0930

    updates picture on about

--- app/src/containters/About/About.jsx
+++ app/src/containters/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./About.module.scss";
-import image from "./../../assets/me.png";
+import image from "./../../assets/profile.png";
 
 const About = () => {
     return (
